9ff3d7b9cf364772a39105e103ff05608301dd76
franck <carnet.franck.paul@gmail.com>
2026-02-10 10:52:06 +0100

    New release 7.0.1

--- _define.php
+++ _define.php
@@ -13,9 +13,9 @@ $this->registerModule(
     'FallSeason',
     'A WP theme designed by Sadish Bala, adapted to Dotclear 2 by Franck Paul',
     'Franck Paul',
-    '7.0',
+    '7.0.1',
     [
-        'date'     => '2025-09-07T14:19:28+0200',
+        'date'     => '2026-02-10T10:52:06+0100',
         'requires' => [['core', '2.36']],
         'type'     => 'theme',
         'overload' => true,
