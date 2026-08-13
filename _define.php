a0b45c7d391c4406a1e9d7448cc0ab4164b7a7dc
franck <contact@open-time.net>
2026-08-13 16:48:49 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -9,20 +9,24 @@
  * @copyright Franck Paul (contact@open-time.net)
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'FallSeason',
-    'A WP theme designed by Sadish Bala, adapted to Dotclear 2 by Franck Paul',
-    'Franck Paul',
-    '7.1',
-    [
-        'date'     => '2026-04-15T18:15:19+0200',
-        'requires' => [['core', '2.36']],
-        'type'     => 'theme',
-        'overload' => true,
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=fallseason',
-        'support'    => 'https://github.com/franck-paul/fallseason',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/fallseason/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'FallSeason',
+        'A WP theme designed by Sadish Bala, adapted to Dotclear 2 by Franck Paul',
+        'Franck Paul',
+        '7.1',
+        [
+            'date'     => '2026-04-15T18:15:19+0200',
+            'requires' => [['core', '2.36']],
+            'type'     => 'theme',
+            'overload' => true,
+
+            'details'    => 'https://open-time.net/?q=fallseason',
+            'support'    => 'https://github.com/franck-paul/fallseason',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/fallseason/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
